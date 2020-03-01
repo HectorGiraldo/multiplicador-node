21fbac0cfd7bc7ffe2959cc718f1066ad72948cd
hector giraldo <hectorgiraldo0210@gmail.com>
2020-03-01 23:32:11 +0100

    commit inicial

---
+++ app.js
@@ -0,0 +1,28 @@
+const argv = require('./config/yargs').argv
+var colors = require('colors');
+
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar')
+
+let comando = argv._[0]
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`archivo creado: ${archivo.green}`))
+            .catch(err => console.log(err));
+        break;
+
+    default:
+        console.log('comando no reconocido');
+        break;
+}
+
+
+
+
+// console.log(argv.base);
